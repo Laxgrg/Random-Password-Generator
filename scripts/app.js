6c6e1bc3ac12aa499d0788d48fedeb9fcd346650
Laxgrg <laxmi.gurung@triosstudent.com>
2024-08-10 21:48:27 -0400

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+const CHARACTERS = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+  "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+  "t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
+  "$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+  "/"];
+
+  let passwordLength = 8;
+
+  function getRanadomChar() {
+    let randomChar = Math.floor(Math.random() * CHARACTERS.length);
+    return CHARACTERS[randomChar];
+  }
+  function generateRandomPassword() {
+    let randomPassword = "";
+    for (let i = 0; i < passwordLength; i++) {
+      randomPassword += getRanadomChar();
+    }
+      return randomPassword;
+  }
+   function displayPasswords() {
+    const passwordOne = generateRandomPassword();
+    const passwordTwo = generateRandomPassword();
+  
+  //displaying password on the input field
+  document.querySelector('.display-generated-pw1').value = passwordOne;
+  document.querySelector('.display-generated-pw2').value = passwordTwo;
+  } 
+  //adding event listener to the "Generate Password" button
+  document.querySelector('.generate-password').addEventListener('click', displayPasswords);
